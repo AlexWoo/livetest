2e7bd6006585315cd952263e033b75e1eba1286a
AlexWoo(武杰) <wj19840501@gmail.com>
2017-06-04 11:22:49 +0800

    add httpflv parser

---
+++ src/httpflv/httpflv.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"media/flv"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s httpflv_url", os.Args[0])
+	}
+
+	res, err := http.Get(os.Args[1])
+	if err != nil {
+		log.Fatalf("GET \"%v\" failed", os.Args[1])
+	}
+	defer res.Body.Close()
+
+	parser := flv.Create(res.Body)
+	parser.Parser()
+}
